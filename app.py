0203aa1e9a986a6801b4c61b1836eb695b798c18
saif khan <72148711+mdsaifk@users.noreply.github.com>
2021-09-16 17:29:40 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,40 @@
+from flask import Flask, request, render_template
+import os
+from flask_cors import CORS, cross_origin
+from yolov4_image import predict
+from werkzeug.utils import secure_filename
+from wsgiref import simple_server
+
+os.putenv('LANG', 'en_US.UTF-8')
+os.putenv('LC_ALL', 'en_US.UTF-8')
+
+app = Flask(__name__)
+CORS(app)
+UPLOAD_FOLDER = 'static/uploads/'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+
+
+@app.route("/", methods=['GET'])
+@cross_origin()
+def home():
+    return render_template('index.html')
+
+    
+
+@app.route("/predict", methods=['POST'])
+@cross_origin()
+def predictRoute():
+    f = request.files['file']
+    filename = secure_filename(f.filename)
+    filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+    f.save(filepath)
+    result = predict(filepath)
+    return render_template("uploaded.html", filepath=filepath)
+
+
+if __name__ == "__main__":
+    port = int(os.getenv("PORT"))
+    host = '0.0.0.0'
+    httpd = simple_server.make_server(host=host,port=port, app=app)
+    httpd.serve_forever()
